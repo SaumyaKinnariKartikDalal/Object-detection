8c006d366ffcc4037e6a49565c4f0d96f3f737dd
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2023-05-11 14:38:17 +0530

    Add files via upload

---
+++ Fruit Basket.js
@@ -0,0 +1,46 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("Fruit Basket.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting objects";
+}
+
+objects = [];
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    textSize(20);
+    if (status !=""){
+        for (i = 0; i < objects.length; i++){
+            fill("#FF0000");
+            strokeWeight(2);
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y-20);
+            noFill();
+            stroke("#FF0000");
+            strokeWeight(5);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
+}
